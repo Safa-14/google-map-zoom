44bdd9ae188361ce9775175a270fce5ccc56f368
safa-dci <safa.bouhlel91@gmail.com>
2020-05-05 16:30:48 +0200

    final

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # google-map-zoom
+In this task i make a zoom google map 
+## Principe
+When we reload the page, we will see first the whole world on the map and then it will zoom smoothly until we see the local of the dci.
---
+++ js/ex.js
@@ -0,0 +1,49 @@
+var map;
+var curZoom = 2
+
+function initMap() {
+    
+    var interval = setInterval(() => {
+        map = new google.maps.Map(document.getElementById('map-div'), {
+            center: {
+                lat: 53.550270,
+                lng: 10.025270
+            },
+            zoom: curZoom, //from 0 to 20
+        });
+
+        curZoom += 1
+
+        if (curZoom === 20) {
+            clearInterval(interval);
+            var marker = new google.maps.Marker({
+                position: {
+                    lat: 53.550270,
+                    lng: 10.025270
+                },
+                map: map,
+
+                title: 'DCI Hamburg',
+                label: 'DCI',
+                draggable: true,
+                animation: google.maps.Animation.DROP 
+            });
+
+            var infowindow = new google.maps.InfoWindow({
+                content: document.querySelector('#windowContainer').innerHTML
+              });
+              
+              //add event to the marker
+              marker.addListener('mouseover', function() {
+                infowindow.open(map, marker);
+              });
+        }
+
+    }, 500);
+
+     //SETUP INFO WINDOW
+  
+
+
+}
+
